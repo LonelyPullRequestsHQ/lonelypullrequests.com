0d03ea9e99ce3bca76abaa2d750e2de78771289c
Dennis de Greef <github@link0.net>
2015-02-08 20:45:10 +0100

    PullRequests now implements Countable and returns self on add method, and private add method, since this is a Value-object which should be immutable

--- src/Domain/PullRequests.php
+++ src/Domain/PullRequests.php
@@ -3,8 +3,9 @@
 namespace LonelyPullRequests\Domain;
 
 use Assert\Assertion as Ensure;
+use Traversable;
 
-final class PullRequests implements \IteratorAggregate
+final class PullRequests implements \IteratorAggregate, \Countable
 {
     /**
      * @var PullRequest[]
@@ -23,13 +24,37 @@ final class PullRequests implements \IteratorAggregate
         }
     }
 
-    public function add(PullRequest $pullRequest)
+    /**
+     * @param PullRequest $pullRequest
+     *
+     * @return PullRequests $this
+     */
+    private function add(PullRequest $pullRequest)
     {
         $this->pullRequests[] = $pullRequest;
+
+        return $this;
     }
 
+    /**
+     * @return Traversable
+     */
     public function getIterator()
     {
         return new \ArrayIterator($this->pullRequests);
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.1.0)<br/>
+     * Count elements of an object
+     * @link http://php.net/manual/en/countable.count.php
+     * @return int The custom count as an integer.
+     * </p>
+     * <p>
+     * The return value is cast to an integer.
+     */
+    public function count()
+    {
+        return sizeof($this->pullRequests);
     }
 }
