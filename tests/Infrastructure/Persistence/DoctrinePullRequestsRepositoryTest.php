6b9c9b17ae678d83cef7cb67d0e37bb992c3bc93
Dennis de Greef <github@link0.net>
2015-02-09 21:23:44 +0100

    DoctrinePullRequestsRepositoryTest

---
+++ tests/Infrastructure/Persistence/DoctrinePullRequestsRepositoryTest.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace LonelyPullRequests\Infrastructure\Persistence;
+
+use LonelyPullRequests\Domain\PullRequest;
+use Mockery;
+use PHPUnit_Framework_TestCase;
+
+class DoctrinePullRequestsRepositoryTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var DoctrinePullRequestsRepository
+     */
+    private $repository;
+    private $entityPersister;
+    private $objects = array();
+
+    public function setUp()
+    {
+        $this->objects = array();
+
+        $this->entityPersister = Mockery::mock('EntityPersistor');
+
+        $unitOfWork = Mockery::mock('Doctrine\ORM\UnitOfWork');
+        $unitOfWork
+            ->shouldReceive('getEntityPersister')
+            ->andReturn($this->entityPersister);
+
+        $entityManager = Mockery::mock('Doctrine\ORM\EntityManagerInterface');
+        $entityManager
+            ->shouldReceive('getUnitOfWork')
+            ->andReturn($unitOfWork);
+        $entityManager
+            ->shouldReceive('persist')
+            ->andReturnNull();
+        $entityManager
+            ->shouldReceive('flush')
+            ->andReturnNull();
+
+        $classMetadata = new \Doctrine\ORM\Mapping\ClassMetadata('\LonelyPullRequests\Domain\PullRequest');
+        $this->repository = new DoctrinePullRequestsRepository($entityManager, $classMetadata);
+    }
+
+    public function testAll()
+    {
+        $this->entityPersister
+            ->shouldReceive('loadAll')
+            ->andReturn($this->objects);
+
+        $pullRequests = $this->repository->all();
+        $this->assertInstanceOf('\LonelyPullRequests\Domain\PullRequests', $pullRequests);
+    }
+
+    public function testAdd()
+    {
+        $pullRequest = PullRequest::fromArray([
+            'title' => 'a',
+            'repositoryName' => 'foo/bar',
+            'url' => 'http://www.example.com/',
+            'loneliness' => 42
+        ]);
+
+        $this->entityPersister
+            ->shouldReceive('loadAll')
+            ->andReturn(array());
+
+        $this->assertEmpty($this->repository->all());
+
+        $this->repository->add($pullRequest);
+
+        $this->entityPersister
+            ->shouldReceive('loadAll')
+            ->andReturn(array($pullRequest));
+
+        // TODO: For some reason, the shouldReceive('loadAll') is not overwritten with new expected array
+        //$this->assertNotEmpty($this->repository->all());
+    }
+}
