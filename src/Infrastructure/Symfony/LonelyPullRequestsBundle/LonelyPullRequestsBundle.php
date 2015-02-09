f5f251fe8f75d49b407b8f28aa4e0e41975e6b63
Dennis de Greef <github@link0.net>
2015-02-09 17:57:12 +0100

    Moved types to yaml configuration in the hope that app/console actions don't require a database now

--- src/Infrastructure/Symfony/LonelyPullRequestsBundle/LonelyPullRequestsBundle.php
+++ src/Infrastructure/Symfony/LonelyPullRequestsBundle/LonelyPullRequestsBundle.php
@@ -8,26 +8,4 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class LonelyPullRequestsBundle extends Bundle
 {
-    /**
-     * Boots the Bundle.
-     */
-    public function boot()
-    {
-        $customTypes = array(
-            'loneliness' => '\LonelyPullRequests\Infrastructure\Symfony\LonelyPullRequestsBundle\Type\LonelinessType',
-            'repositoryName' => '\LonelyPullRequests\Infrastructure\Symfony\LonelyPullRequestsBundle\Type\RepositoryNameType',
-            'summary' => '\LonelyPullRequests\Infrastructure\Symfony\LonelyPullRequestsBundle\Type\SummaryType',
-            'url' => '\LonelyPullRequests\Infrastructure\Symfony\LonelyPullRequestsBundle\Type\UrlType'
-        );
-
-        /** @var EntityManager $em */
-        $em = $this->container->get('doctrine.orm.default_entity_manager');
-        $conn = $em->getConnection();
-
-        // Add value-objects as Doctrine DBAL custom types
-        foreach($customTypes as $typeName => $typeClass) {
-            Type::addType($typeName, $typeClass);
-            $conn->getDatabasePlatform()->registerDoctrineTypeMapping($typeName, $typeName);
-        }
-    }
 }
