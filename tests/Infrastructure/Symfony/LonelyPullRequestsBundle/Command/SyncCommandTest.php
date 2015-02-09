622e786491c8f7331abe22c6dba2408abf24c916
Dennis de Greef <github@link0.net>
2015-02-09 22:04:16 +0100

    Added SyncCommand for syncing data from github

---
+++ tests/Infrastructure/Symfony/LonelyPullRequestsBundle/Command/SyncCommandTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace LonelyPullRequests\Infrastructure\Symfony\LonelyPullRequestsBundle\Command;
+
+use Mockery;
+use PHPUnit_Framework_TestCase;
+
+class SyncCommandTest extends PHPUnit_Framework_TestCase
+{
+    public function testConfigTreeBuilder()
+    {
+        $inputInterface = Mockery::mock('Symfony\Component\Console\Input\InputInterface');
+        $outputInterface = Mockery::mock('Symfony\Component\Console\Output\OutputInterface');
+
+        $inputInterface
+            ->shouldReceive('bind')
+            ->andReturnNull();
+
+        $inputInterface
+            ->shouldReceive('isInteractive')
+            ->andReturn(false);
+        $inputInterface
+            ->shouldReceive('validate')
+            ->andReturn();
+        $outputInterface
+            ->shouldReceive('writeln')
+            ->andReturn();
+
+        $command = new SyncCommand();
+        $command->run($inputInterface, $outputInterface);
+    }
+}
