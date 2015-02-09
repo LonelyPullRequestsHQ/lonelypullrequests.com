fbdfa3ea1c2f79f87b925348d754cc5c24330168
Dennis de Greef <github@link0.net>
2015-02-09 18:04:12 +0100

    Removed obsolete imports from bundle

--- src/Infrastructure/Symfony/LonelyPullRequestsBundle/LonelyPullRequestsBundle.php
+++ src/Infrastructure/Symfony/LonelyPullRequestsBundle/LonelyPullRequestsBundle.php
@@ -2,8 +2,6 @@
 
 namespace LonelyPullRequests\Infrastructure\Symfony\LonelyPullRequestsBundle;
 
-use Doctrine\DBAL\Types\Type;
-use Doctrine\ORM\EntityManager;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class LonelyPullRequestsBundle extends Bundle
